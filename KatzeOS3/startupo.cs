e2da7683700b37958a2413aa655bc3d426f610ab
xap12 <xapax012@gmail.com>
2024-07-13 13:44:21 +0330

    Create startupo.cs

---
+++ KatzeOS3/startupo.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KatzosT
+{
+    public class startupo
+    {
+        public static void banner()
+        {
+            Console.WriteLine(@"     ___  __       
+ ___|\  \|\  \     
+|\__\ \  \/  /|_   
+\|__|\ \   ___  \  
+    __\ \  \\ \  \ 
+   |\__\ \__\\ \__\
+   \|__|\|__| \|__|
+                   
+                   
+                   ");
+
+        }
+    }
+}
